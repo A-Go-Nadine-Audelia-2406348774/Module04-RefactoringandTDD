1204319442b35a74dd990348ac715dde662cf709
Nad-0 <go.nadine@cs.ui.ac.id>
2026-03-10 11:19:01 +0700

    Add Order model skeleton

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Order.java
@@ -1,12 +1,18 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import java.util.List;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Builder
+@Getter
 public class Order {
     private String id;
     private List<Product> products;
     private long orderTime;
     private String author;
+    @Setter
     private String status;
 
     public Order(String id, List<Product> products, Long orderTime, String author) {
